ab978951085a3764af05289ff5a13969917ca8d4
Waelhouidi <waelhwidi0@gmail.com>
2024-11-23 22:47:35 +0100

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,10 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './cards/cards.component';
 import { ProjectListComponent } from './projects-list/projects-list.component';
-import { SigninComponent } from './signin/signin.component';
-import { RegisterComponent } from './register/register.component';
+
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'project', component:ProjectsComponent},
     {path: 'project-list', component:ProjectListComponent},
-  {path:'signin', component:SigninComponent},
-  {path:'register', component:RegisterComponent},
 ];
