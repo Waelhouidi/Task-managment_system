afa9cbd8c0ab8300a1011925b43630046372e1f1
Waelhouidi <waelhwidi0@gmail.com>
2024-11-24 12:57:16 +0100

    fix bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './cards/cards.component';
 import { ProjectListComponent } from './projects-list/projects-list.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
-  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  { path: "", component: ProjectListComponent, canActivate: [AuthGuard] },
+  {path:"login",component:LoginComponent},
     {path: 'project', component:ProjectsComponent},
     {path: 'project-list', component:ProjectListComponent},
 ];
